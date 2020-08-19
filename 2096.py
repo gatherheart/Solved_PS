84a791e1f62fa201360eddf62924082f39f4f98b
gatherheart <imonit@naver.com>
2020-08-20 01:26:02 +0900

    [ADD] 2096 solved

--- 2096.py
+++ 2096.py
@@ -11,77 +11,58 @@ MIN_CACHE_VALUE = -MAX_CACHE_VALUE
 '''VARIABLES'''
 # N(1 ≤ N ≤ 100,000)
 N, M = 0, 3
-min_score, max_score = 0, 0
+min_score, max_score = MAX_CACHE_VALUE, MIN_CACHE_VALUE
 arr = []
 cache = []
-
 '''UTILS'''
 
-def print_arr(_arr, N, M):
-    for i in range(N):
-        for j in range(M):
-            print(_arr[i][j], end=" ")
-        print()
-
-def BFS(start, N, M, cache, arr):
+def go_down(cache, arr):
     
-    # Initialize Cache at the start position
-    cache[start[0]][start[1]][0] = arr[start[0]][start[1]]
-    cache[start[0]][start[1]][1] = arr[start[0]][start[1]]
+    # (Min, Max) CACHE for 2 rows X 3 columns 
+    cache.append([[MAX_CACHE_VALUE, MIN_CACHE_VALUE] for i in range(3)])
+    for col, score in enumerate(arr[1]):
 
-    queue = [start]
+        if col == LEFTMOST:
+            # Update Min
+            cache[1][0][0] = min([cache[1][0][0], cache[0][0][0] + score, cache[0][1][0] + score])
+            # Update Max
+            cache[1][0][1] = max([cache[1][0][1], cache[0][0][1] + score, cache[0][1][1] + score])
 
-    while queue:
+        elif col == MIDDLE:
+            cache[1][1][0] = min([cache[1][1][0], cache[0][0][0] + score, \
+                cache[0][1][0] + score, cache[0][2][0] + score])
+            cache[1][1][1] = max([cache[1][1][1], cache[0][0][1] + score, \
+                cache[0][1][1] + score, cache[0][2][1] + score])
+    
+        elif col == RIGHTMOST:
+            cache[1][2][0] = min([cache[1][2][0], cache[0][1][0] + score, cache[0][2][0] + score])
+            cache[1][2][1] = max([cache[1][2][1], cache[0][1][1] + score, cache[0][2][1] + score])
 
-        x, y = queue.pop(0)
-        if x == N - 1:
-            continue
-
-        possible_new_y = [y]
-
-        if y == LEFTMOST:    
-            possible_new_y.append(y+1)
-        elif y == MIDDLE:
-            possible_new_y.append(y-1)
-            possible_new_y.append(y+1)
-        elif y == RIGHTMOST:
-            possible_new_y.append(y-1)
-
-        for new_y in possible_new_y:
-            # Update Min at new position
-            cache[x+1][new_y][0] = min(cache[x+1][new_y][0], cache[x][y][0] + arr[x+1][new_y])
-            cache[x+1][new_y][1] = max(cache[x+1][new_y][1], cache[x][y][1] + arr[x+1][new_y])
-            queue.append((x+1, new_y))
-
-    return
 
 '''INPUT'''
-N = int(sys.stdin.readline())
-
-for i in range(N):
-    arr.append(list(map(int, sys.stdin.readline().split())))
-
-# (Min, Max)
-cache = [[[MAX_CACHE_VALUE, MIN_CACHE_VALUE] for j in range(M)] for i in range(N)]
-
 '''COMPUTE'''
 
-for i in range(M):
-    BFS((0, i), N, M, cache, arr)
+N = int(sys.stdin.readline())
 
-min_score = cache[-1][0][0]
-max_score = cache[-1][0][1]
+arr.append(list(map(int, sys.stdin.readline().split())))
+first_cache = list(map(lambda x: [x, x], arr[0]))
+cache.append(first_cache)
 
+for i in range(N-1):
+    arr.append(list(map(int, sys.stdin.readline().split())))
+    go_down(cache, arr)
+    
+    cache.pop(0)
+    arr.pop(0)
+
+'''OUTPUT'''
 
 for score in cache[-1]:
     if min_score > score[0]:
         min_score = score[0]
-    
+
     if max_score < score[1]:
         max_score = score[1]
-
-
-'''OUTPUT'''
 
 print(max_score, min_score)
 
@@ -101,5 +82,22 @@ CASE 2.
 4 9 0
 1 2 3
 4 5 6
+
+27 12
+
+CASE 3.
+
+4
+1 2 3
+4 10 6
+10 10 0
+
+4
+1 2 3 => [1, 1] [2, 2] [3, 3]
+4 10 6 => [5, 6] [11, 13] [8, 9]
+10 10 0 => [15, 23] [15, 23] [8, 13]
+
+
+
 
 '''
