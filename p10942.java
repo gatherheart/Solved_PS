d1bf5fbe4636e9381c564c3d87ff5bdaa22d08b4
gatherheart <imonit@naver.com>
2020-10-22 13:54:04 +0900

    [ADD] 2169.java solved but needs optimization

--- p10942.java
+++ p10942.java
@@ -6,19 +6,18 @@ import java.util.StringTokenizer;
 public class p10942 { 
 
     final private static int TRUE = 1;
-    final private static int FALSE = 0;
 
     private static void buildDP(int N, int[] word, int[][] dp){
         for (int i = 1; i < N + 1; i++)
-            dp[1][i] = TRUE;
+            dp[i][i] = TRUE;
         for (int i = 1; i < N; i++)
             if (word[i] == word[i+1])
-                dp[2][i] = TRUE;
+                dp[i][i+1] = TRUE;
 
-        for (int i = 3; i < N + 1; i++){
-            for (int j = 1; i+j-1 < N + 1; j++){
-                if (word[j] == word[i+j-1] && dp[i-2][j+1] == TRUE){
-                    dp[i][j] = TRUE;
+        for (int i = 2; i < N; i++){
+            for (int j = 1; i+j < N + 1; j++){
+                if (word[j] == word[i+j] && dp[j+1][i+j-1] == TRUE){
+                    dp[j][i+j] = TRUE;
                 }
             }
         }   
@@ -47,7 +46,7 @@ public class p10942 {
             st = new StringTokenizer(br.readLine());
             int S = Integer.parseInt(st.nextToken());
             int E = Integer.parseInt(st.nextToken());
-            sb.append(dp[E-S+1][S]+"\n");
+            sb.append(dp[S][E]+"\n");
         }
         System.out.print(sb.toString());
     }
